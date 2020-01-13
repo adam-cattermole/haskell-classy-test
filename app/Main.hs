375f3b67edd8c18d414d6311e4695250da2d0c91
Adam Cattermole <a.d.cattermole@gmail.com>
2020-01-13 14:59:18 +0000

    Change to use typeclasses

--- app/Main.hs
+++ app/Main.hs
@@ -11,84 +11,39 @@ import Control.Lens
 main :: IO ()
 main = do
     -- Building some configs, needs to be have no specificity
-    let confone = AppConfig $ StreamConfOne $ ConfOne 1 5000000
+    let confone = AppConfig (StreamConfOne $ ConfOne 1 5000000) 5
         conftwo = defaultConfig
     -- User needs to be able to call function like this
-    simpleNode conftwo strFn
+    simpleNode confone strFn
 
 
 defaultConfig :: AppConfig
-defaultConfig = AppConfig $ StreamConfTwo $ ConfTwo 0 100000
+defaultConfig = AppConfig (StreamConfTwo $ ConfTwo 0 100000) 10
 
 
 simpleNode :: AppConfig -> (Int -> (Int, Int)) -> IO ()
 simpleNode config fn = do
-    let buildFn = case config ^. ingress of
-                    StreamConfOne _ -> runExceptT (runReaderT (unApp buildStream) config)
-                    StreamConfTwo _ -> runExceptT (runReaderT (unApp buildStream') config)
-    x <- buildFn
+    x <- runExceptT (runReaderT (unApp $ nodeInternal fn) config)
     case x of
-        Left  e  -> return ()
-        Right r -> do
-            let result = map fn r
-            mapM_ (\x -> liftIO $ print x) result
+        Left  e -> print "yikes error" >> return ()
+        Right r -> print "done"        >> return () 
+
+
+nodeInternal :: (MonadReader r m,
+                 HasAppConfig r,
+                 MonadIO m)
+             => (Int -> (Int, Int))
+             -> m ()
+nodeInternal fn = do
+    c <- ask
+    -- can use higher level config
+    liftIO $ print (c ^. globalconf)
+    -- extract ingress conf and run
+    stream <- liftIO $ ingressFn (c ^. ingress)
+    let result = map fn stream
+    liftIO $ mapM_ print result
 
 
 -- Fake stream op function
 strFn :: Int -> (Int, Int)
 strFn x = (x, x*2)
-
-
--- Function that builds dependent on ConfOne
-buildStream :: (MonadReader r m,
-                HasConfOne r,
-                MonadIO m) => m [Int]
-buildStream = do
-    c <- ask
-    liftIO $ go (c ^. delayFive) (c ^. initZero)
-    where
-        go delay i = unsafeInterleaveIO $ do
-            threadDelay delay
-            xs <- go delay (i+1)
-            return (i:xs)
-
--- Function that builds dependent on ConfTwo
-buildStream' :: (MonadReader r m,
-                HasConfTwo r,
-                MonadIO m) => m [Int]
-buildStream' = do
-    c <- ask
-    liftIO $ go (c ^. delayOne) (c ^. initOne)
-    where
-        go delay i = unsafeInterleaveIO $ do
-            threadDelay delay
-            xs <- go delay (i+1)
-            return (i*2:xs)
-
-
--- Non-Classy method but works
-simpleNode' :: AppConfig -> (Int -> (Int, Int)) -> IO ()
-simpleNode' conf fn = do
-    x <- case conf ^. ingress of
-            StreamConfOne co -> buildStreamNC  (_delayFive co)  (_initZero co)
-            StreamConfTwo ct -> buildStreamNC' ( _delayOne ct) (_initOne ct)
-    let result = map fn x
-    mapM_ print result
-
-
-buildStreamNC :: Int -> Int -> IO [Int]
-buildStreamNC = go
-    where
-        go delay i = unsafeInterleaveIO $ do
-            threadDelay delay
-            xs <- go delay (i+1)
-            return (i:xs)
-
-
-buildStreamNC' :: Int -> Int -> IO [Int]
-buildStreamNC' = go
-    where
-        go delay i = unsafeInterleaveIO $ do
-            threadDelay delay
-            xs <- go delay (i+1)
-            return (i*2:xs)
--- src/Types.hs
+++ src/Types.hs
@@ -6,6 +6,8 @@ module Types where
 import Control.Monad.Reader
 import Control.Monad.Except
 import Control.Lens.Combinators (makeClassy, makeClassyPrisms)
+import System.IO.Unsafe (unsafeInterleaveIO)
+import Control.Concurrent (threadDelay)
 
 data ConfOne = ConfOne
     { _initZero :: Int
@@ -26,7 +28,8 @@ makeClassyPrisms ''StreamConfig
 data AppError = AppError
 
 data AppConfig = AppConfig
-    { _ingress :: StreamConfig
+    { _ingress    :: StreamConfig
+    , _globalconf :: Int
     }
 makeClassy ''AppConfig
 
@@ -41,3 +44,27 @@ newtype App a =
         MonadError AppError,
         MonadIO
     )
+
+class Controller a where
+    ingressFn :: a -> IO [Int]
+    -- egressFn :: a -> IO ()
+
+instance Controller StreamConfig where
+    ingressFn (StreamConfOne c) = buildS  c
+    ingressFn (StreamConfTwo c) = buildS' c
+
+buildS :: ConfOne -> IO [Int]
+buildS co = go (_delayFive co)  (_initZero co)
+    where
+        go delay i = unsafeInterleaveIO $ do
+            threadDelay delay
+            xs <- go delay (i+1)
+            return (i:xs)
+
+buildS' :: ConfTwo -> IO [Int]
+buildS' ct = go ( _delayOne ct) (_initOne ct)
+    where
+        go delay i = unsafeInterleaveIO $ do
+            threadDelay delay
+            xs <- go delay (i+1)
+            return (i*2:xs)
